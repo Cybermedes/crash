a2f4b5d61190b4efe1d56d049920081365cba3f8
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-23 15:58:51 -0300

    feat: add the list option
    The script now has the list option, to print files from a directory. Help section moved to a separated file

--- file-janitor.sh
+++ file-janitor.sh
@@ -6,22 +6,45 @@
 
 function print_information {
     echo "File Janitor, $(date +"%Y")"
-    echo "Powered by $(bash --version | head -n1)"
+    echo "Powered by Bash"
 }
 
 function print_help {
-    echo
-    echo "File Janitor is a tool to discover files and clean directories"
-    echo
-    echo "Usage: file-janitor [option] [file_path]"
-    echo
-    echo "    options:"
-    echo "        help        displays this help file"
+    cat file-janitor-help.txt
 }
 
-if [ $# -eq 1 ] && [ "$1" == "help" ]; then
+print_files() {
+    echo
+    # One file path provided
+    if [ $# -eq 1 ] && [ -n "$1" ]; then
+        # Check if file exists
+        if [ ! -e "$1" ]; then
+            echo "$1 is not found"
+        # Check if file is a directory
+        elif [ ! -d "$1" ]; then
+            echo "$1 is not a directory"
+        # Print a files inside the directory
+        else
+            echo "Listing files in $1"
+            echo
+            ls -1A "$1"
+        fi
+    # Script executed with the list option, no file path provided
+    else
+        echo "Listing files in the current directory"
+        echo
+        ls -1A
+    fi
+}
+
+if [ $# -ge 1 ] && [ "$1" == "help" ]; then
     print_information
     print_help
+elif [ $# -ge 1 ] && [ "$1" == "list" ]; then
+    print_information
+    print_files "$2"
 else
     print_information
+    echo
+    echo "Type file-janitor.sh help to see available options"
 fi
