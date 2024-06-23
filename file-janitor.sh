7fa1ee090b7812cd507b1bceae748702ed2d252a
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-22 22:38:37 -0300

    feat: start file janitor project

---
+++ file-janitor.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# File Janitor
+# A Hyperskill project exercise
+# Available at https://hyperskill.org/projects/330?track=26
+
+echo "File Janitor, $(date +"%Y")"
+echo "Powered by Bash"
