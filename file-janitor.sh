718da3a239872c03e714c8250eb6b4114ea0249e
Lucas Paiva <lpaivamatiazzi@gmail.com>
2024-06-23 14:33:20 -0300

    feat: add help subcommand
    The script will print a help section information with the help subcommand. In case, no subcommand matches it, print it's basic description

--- file-janitor.sh
+++ file-janitor.sh
@@ -4,5 +4,24 @@
 # A Hyperskill project exercise
 # Available at https://hyperskill.org/projects/330?track=26
 
-echo "File Janitor, $(date +"%Y")"
-echo "Powered by Bash"
+function print_information {
+    echo "File Janitor, $(date +"%Y")"
+    echo "Powered by $(bash --version | head -n1)"
+}
+
+function print_help {
+    echo
+    echo "File Janitor is a tool to discover files and clean directories"
+    echo
+    echo "Usage: file-janitor [option] [file_path]"
+    echo
+    echo "    options:"
+    echo "        help        displays this help file"
+}
+
+if [ $# -eq 1 ] && [ "$1" == "help" ]; then
+    print_information
+    print_help
+else
+    print_information
+fi
